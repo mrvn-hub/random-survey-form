f98e197bd4a82b217310b0d787d6c1b17afa7a1f
Marvin Ndlovu <marvin.m.ndlovu@gmail.com>
2023-08-06 20:29:31 +0200

    .gitignore is  working

--- form/urls.py
+++ form/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
 	path("", views.index, name="index"),
-	path("submit", views.register_session, name="register_session")
 ]
--- form/views.py
+++ form/views.py
@@ -5,52 +5,26 @@ from django.shortcuts import render
 from .models import Form
 from .myform import MyForm
 from django.template import loader
-import json 
-import pyrebase
-import math
+import json, pyrebase, math, os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 
 
 firebaseConfig = {
-	"apiKey": "AIzaSyDRww6gG2JiC7x5C5rqPNiqkPKThLjgyBQ",
-	"authDomain": "random-survey-form.firebaseapp.com",
-	"projectId": "random-survey-form",
-	"storageBucket": "random-survey-form.appspot.com",
-	"messagingSenderId": "764014336225",
-	"appId": "1:764014336225:web:3a02a4b3bf043282693fa0",
-	"databaseURL": "https://random-survey-form-default-rtdb.europe-west1.firebasedatabase.app"
+	"apiKey": os.getenv("FIREBASE_API_KEY"),
+	"authDomain": os.getenv("FIREBASE_AUTH_DOMAIN"),
+	"projectId": os.getenv("FIREBASE_PROJECT_ID"),
+	"storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET"),
+	"messagingSenderId": os.getenv("FIREBASE_MESSAGING_SENDER_ID"),
+	"appId": os.getenv("FIREBASE_APP_ID"),
+	"databaseURL": os.getenv("FIREBASE_DB_URL")
 }
 
 survey = pyrebase.initialize_app(firebaseConfig)
 db = survey.database()
-
-
-# analytics = getAnalytics(survey);
-# Application views.
-
-def get_session(request):
-	if request.method=="POST":
-		form = MyForm(request.POST)
-		if form.is_valid():
-			return HttpResponseRedirect("/thanks")
-	else:
-		form = MyForm()
-	return render(request, "index.html", {"form": form})
-
-
-def register_session(request):
-	if request.method=="POST":
-		# body = json.loads(request.body.decode("utf-8"))
-		print(body)
-		new_name = body.get("name")
-		new_agreement = body.get("agreement")
-		new_date = body.get("current_date")
-		new_time = body.get("current_time")
-		
-		new_session = Form(name=new_name, agreement=new_agreement, current_date=new_date, current_time=new_time)
-		new_session.save()
-		return HttpResponse(status=200)
-	else:
-		return HttpResponseNotAllowed("Not supported")
 
 def index(request):
 	myform = MyForm()
@@ -58,15 +32,11 @@ def index(request):
 	all_sess = db.child("sessions").get().val()
 	session_count = all_sess.__len__()
 	total_count = session_count + 17
-	# print(g)
 	if request.method=="POST":
 		print(request.POST)
-		# print("G shit: ", g.__len__())#.val().__len__())
 		form = MyForm(request.POST)
 		if form.is_valid():
-			# post = (request.POST)
-			# pist = json.loads({"yes": "ans"})
-			# merged_dict = {post** + pist**}
+
 			data ={
 				json.dumps(form.cleaned_data),
 				"mess", "HU"
@@ -79,7 +49,6 @@ def index(request):
 	template = loader.get_template("index.html")
 	context = {
 		"myform": myform,
-		# "session": session,
 		"count": session_count,
 		"total_count": total_count
 	}
