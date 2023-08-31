aa01b6776ae9f277f80170ecb89a54732f270f42
Marvin Ndlovu <marvin.m.ndlovu@gmail.com>
2023-08-31 15:26:57 +0200

    fix: static files

--- surveyform/settings.py
+++ surveyform/settings.py
@@ -118,8 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    "form/static"
+]
 STATIC_ROOT = BASE_DIR / "staticfiles/"
-STATIC_URL = 'static/'
+STATIC_URL = 'statics/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
